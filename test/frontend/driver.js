ab60262cc9b8e13cbc05a75a9c59bf6385e31d88
Abhinav Adduri <abhinadduri@gmail.com>
2017-07-17 09:49:24 -0700

    testing circle

--- test/frontend/driver.js
+++ test/frontend/driver.js
@@ -1,22 +1,22 @@
-var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
+const webdriver = require('selenium-webdriver');
+const path = require('path');
+const until = webdriver.until;
 
-var driver = new webdriver.Builder()
+const driver = new webdriver.Builder()
   .forBrowser('firefox')
   .build();
 
-driver.get('file:///' + __dirname + '/frontend.test.html');
+driver.get(path.join('file:///', __dirname, '/frontend.test.html'));
 driver.wait(until.titleIs('Mocha Tests'), 1000);
 driver.wait(until.titleMatches(/^[0-1]$/), 10000);
 
 driver.getTitle().then(title => {
   driver.quit().then(() => {
-    if (title === "0") {
-      process.exit(0);
+    if (title === '0') {
+      // process.exit(0);
     } else {
-      throw new Error(`Frontend tests are failing. ` + 
-                `Please open the frontend.test.html file in a browser.`);
+      throw new Error('Frontend tests are failing. ' + 
+                'Please open the frontend.test.html file in a browser.');
     }
   })
 })
