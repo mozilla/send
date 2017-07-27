6b291ae01207e0b72b41fb4fb80ffeca00dbbcb0
Erica Wright <ericaewright@gmail.com>
2017-07-27 11:13:59 -0400

    show error page on firefox v49 and below

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -3,8 +3,24 @@ window.Raven.config(window.dsn).install();
 window.dsn = undefined;
 
 const testPilotGA = require('testpilot-ga');
+const {gcmCompliant, sendEvent} = require('./utils');
 window.analytics = new testPilotGA({
   an: 'Firefox Send',
   ds: 'web',
   tid: window.trackerId
 });
+
+gcmCompliant().catch(err => {
+  $('#page-one').attr('hidden', true);
+  $('#download').attr('hidden', true);
+  sendEvent('sender', 'unsupported', {
+    cd6: err
+  }).then(() => {
+    location.replace('/unsupported');
+  });
+});
+
+if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1 &&
+    parseInt(navigator.userAgent.toLowerCase().match(/firefox\/*([^\n\r]*)\./)[1]) <= 49) {
+    location.replace('/unsupported');
+}
