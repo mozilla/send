7a53ed96ed5b6c439456172335bc51e2f00eb01c
Abhinav Adduri <abhinadduri@gmail.com>
2017-07-27 10:01:39 -0700

    moved back to common

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -9,3 +9,24 @@ window.analytics = new testPilotGA({
   ds: 'web',
   tid: window.trackerId
 });
+
+const isSender = location.pathname.includes('/download');
+
+gcmCompliant().catch(err => {
+  $('#page-one').attr('hidden', true);
+  $('#download').attr('hidden', true);
+  sendEvent(isSender ? 'sender' : 'recipient', 'unsupported', {
+    cd6: err
+  }).then(() => {
+    location.replace('/unsupported');
+  });
+});
+
+if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1 &&
+    parseInt(navigator.userAgent.toLowerCase().match(/firefox\/*([^\n\r]*)\./)[1]) <= 49) {
+    sendEvent(isSender ? 'sender' : 'recipient', 'unsupported', {
+      cd6: new Error('Firefox is outdated.')
+    }).then(() => {
+      location.replace('/unsupported');
+    });
+}
