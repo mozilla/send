4f3c2498a60fc24867af588d981d5c24d94987ee
Peter deHaan <peter@deseloper.com>
2017-08-01 20:15:45 -0700

    Add get-prod-locales and lint-locales scripts

--- scripts/get-prod-locales.js
+++ scripts/get-prod-locales.js
@@ -1,45 +1,49 @@
-/* eslint-disable no-console */
+#!/usr/bin/env node
 
-const { exec } = require('child_process');
+const cp = require('child_process');
+const { promisify } = require('util');
 const fs = require('fs');
-
 const pkg = require('../package.json');
+
 const availableLanguages = pkg.availableLanguages.sort();
+const exec = promisify(cp.exec);
 
-const compareLocales =
-  'compare-locales l10n.toml . `ls public/locales` --data=json';
+const arrayDiff = (current, package) =>
+  current.filter(locale => !package.includes(locale));
 
-exec(compareLocales, (err, stdout, stderr) => {
-  if (err) {
+const cmd = 'compare-locales l10n.toml . `ls public/locales` --data=json';
+
+exec(cmd)
+  .then(({ stdout }) => JSON.parse(stdout))
+  .then(({ summary }) => {
+    const locales = Object.keys(summary)
+      .filter(locale => {
+        const loc = summary[locale];
+        const hasMissing = loc.hasOwnProperty('missing');
+        const hasErrors = loc.hasOwnProperty('errors');
+        return !hasMissing && !hasErrors;
+      })
+      .sort();
+
+    if (locales.join(',') !== availableLanguages.join(',')) {
+      const missingLanguages = arrayDiff(locales, availableLanguages);
+
+      console.log('current 100%:', JSON.stringify(locales));
+      console.log('package.json:', JSON.stringify(availableLanguages));
+      console.log('missing prod:', JSON.stringify(missingLanguages));
+
+      if (process.argv.includes('--write')) {
+        const pkgPath = require.resolve('../package.json');
+        pkg.availableLanguages = locales;
+        const str = JSON.stringify(pkg, null, 2) + '\n';
+        console.log('Updating /package.json availableLanguages');
+        fs.writeFileSync(pkgPath, str, 'utf-8');
+      }
+    } else {
+      console.log('Production locales are up to date!');
+    }
+  })
+  .catch(err => {
     console.error(err);
     process.exit(1);
-  }
-  const missingLocales = (current, package) =>
-    current.filter(locale => !package.includes(locale));
-  const { summary } = JSON.parse(stdout);
-  const locales = Object.keys(summary)
-    .filter(locale => {
-      const loc = summary[locale];
-      const hasMissing = loc.hasOwnProperty('missing');
-      const hasErrors = loc.hasOwnProperty('errors');
-      return !hasMissing && !hasErrors;
-    })
-    .sort();
-
-  if (locales.join(',') !== availableLanguages.join(',')) {
-    const missing = missingLocales(locales, availableLanguages);
-    console.log('current 100%:', JSON.stringify(locales));
-    console.log('package.json:', JSON.stringify(availableLanguages));
-    console.log('missing prod:', JSON.stringify(missing));
-
-    if (process.argv.includes('--write')) {
-      const pkgPath = require.resolve('../package.json');
-      pkg.availableLanguages = locales;
-      const str = JSON.stringify(pkg, null, 2) + '\n';
-      console.log('Updating /package.json availableLanguages');
-      fs.writeFileSync(pkgPath, str, 'utf-8');
-    }
-  } else {
-    console.log('Production locales are up to date!');
-  }
-});
+  });
---
+++ scripts/lint-locales.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const cp = require('child_process');
+const { promisify } = require('util');
+const pkg = require('../package.json');
+const conf = require('../server/config');
+
+const exec = promisify(cp.exec);
+const cmd = `compare-locales l10n.toml . ${getLocales()} --data=json`;
+
+console.log(cmd);
+
+exec(cmd)
+  .then(({ stdout }) => JSON.parse(stdout))
+  .then(({ details }) => filterErrors(details))
+  .then(results => {
+    if (results.length) {
+      results.forEach(({ locale, data }) => {
+        console.log(locale);
+        data.forEach(msg => console.log(`- ${msg}`));
+        console.log('');
+      });
+      process.exit(2);
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+function filterErrors(details) {
+  return Object.keys(details)
+    .sort()
+    .map(locale => {
+      const data = details[locale]
+        .filter(item => item.hasOwnProperty('error'))
+        .map(({ error }) => error);
+      return { locale, data };
+    })
+    .filter(({ data }) => data.length);
+}
+
+function getLocales() {
+  // If we're in a "production" env (or passed the `--production` flag), only
+  // check the locales from the package.json file's `availableLanguages` array.
+  if (conf.env === 'production' || process.argv.includes('--production')) {
+    return pkg.availableLanguages.sort().join(' ');
+  }
+  // Lint all the locales.
+  return '`ls public/locales`';
+}
