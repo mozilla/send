81442bb6f2c28a33d8a5fbb67ff1a2b1e53f79c4
Danny Coates <dannycoates@gmail.com>
2017-09-14 12:15:08 -0700

    set default server states for fira and fileInfo

--- server/state.js
+++ server/state.js
@@ -19,6 +19,8 @@ module.exports = function(req) {
       uploadWindowStyle: 'upload-window',
       uploadButtonStyle: 'browse btn'
     },
+    fira: false,
+    fileInfo: {},
     layout
   };
 };
