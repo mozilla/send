0f8c3caf18e275b121e613924187103f33d4a030
Danny Coates <dannycoates@gmail.com>
2017-12-19 10:06:45 -0800

    fixed untranslated strings breaking the site

--- build/fluent_loader.js
+++ build/fluent_loader.js
@@ -1,26 +1,45 @@
 const { MessageContext } = require('fluent');
+const fs = require('fs');
 
 function toJSON(map) {
   return JSON.stringify(Array.from(map));
+}
+
+function merge(m1, m2) {
+  const result = new Map(m1);
+  for (const [k, v] of m2) {
+    result.set(k, v);
+  }
+  return result;
 }
 
 module.exports = function(source) {
   const localeExp = this.options.locale || /([^/]+)\/[^/]+\.ftl$/;
   const result = localeExp.exec(this.resourcePath);
   const locale = result && result[1];
-  // pre-parse the ftl
-  const context = new MessageContext(locale);
-  context.addMessages(source);
   if (!locale) {
     throw new Error(`couldn't find locale in: ${this.resourcePath}`);
   }
+  // load default language and "merge" contexts
+  // TODO: make this configurable
+  const en_ftl = fs.readFileSync(
+    require.resolve('../public/locales/en-US/send.ftl'),
+    'utf8'
+  );
+  const en = new MessageContext('en-US');
+  en.addMessages(en_ftl);
+  // pre-parse the ftl
+  const context = new MessageContext(locale);
+  context.addMessages(source);
+
+  const merged = merge(en._messages, context._messages);
   return `
 module.exports = \`
 if (typeof window === 'undefined') {
   var fluent = require('fluent');
 }
 var ctx = new fluent.MessageContext('${locale}', {useIsolating: false});
-ctx._messages = new Map(${toJSON(context._messages)});
+ctx._messages = new Map(${toJSON(merged)});
 function translate(id, data) {
   var msg = ctx.getMessage(id);
   if (typeof(msg) !== 'string' && !msg.val && msg.attrs) {
