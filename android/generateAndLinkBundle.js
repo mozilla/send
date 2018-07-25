5a6054d2013662f847d5915616e5091cf5761cd6
Donovan Preston <donovanpreston@gmail.com>
2018-07-25 10:30:35 -0400

    It looks like we no longer build the runtime module.

--- android/generateAndLinkBundle.js
+++ android/generateAndLinkBundle.js
@@ -1,25 +1,21 @@
 const child_process = require('child_process');
 const fs = require('fs');
+const path = require('path');
 
 child_process.execSync('npm run build');
 
-const json_string = fs.readFileSync('../../dist/manifest.json');
+const prefix = path.join('..', '..', 'dist');
+const json_string = fs.readFileSync(path.join(prefix, 'manifest.json'));
 const manifest = JSON.parse(json_string);
 
 const android_filename = manifest['android.js'];
 fs.writeFileSync(
   'src/main/assets/android.js',
-  fs.readFileSync(`../../dist/${android_filename}`)
+  fs.readFileSync(`${prefix}${android_filename}`)
 );
 
 const vendor_filename = manifest['vendor.js'];
 fs.writeFileSync(
   'src/main/assets/vendor.js',
-  fs.readFileSync(`../../dist/${vendor_filename}`)
-);
-
-const runtime_filename = manifest['runtime.js'];
-fs.writeFileSync(
-  'src/main/assets/runtime.js',
-  fs.readFileSync(`../../dist/${runtime_filename}`)
+  fs.readFileSync(`${prefix}${vendor_filename}`)
 );
