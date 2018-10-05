9e8e604024e13adbc9bad49325e1396dd74f36b0
Danny Coates <dannycoates@gmail.com>
2018-10-05 11:01:58 -0700

    stubbed in gcs (untested)

---
+++ server/storage/gcs.js
@@ -0,0 +1,37 @@
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
+
+class GCSStorage {
+  constructor(config, log) {
+    this.bucket = storage.bucket(config.gcs_bucket);
+    this.log = log;
+  }
+
+  async length(id) {
+    const data = await this.bucket.file(id).getMetadata();
+    return data[0].size;
+  }
+
+  getStream(id) {
+    return this.bucket.file(id).createReadStream();
+  }
+
+  set(id, file) {
+    return new Promise((resolve, reject) => {
+      file
+        .pipe(this.bucket.file(id).createWriteStream())
+        .on('error', reject)
+        .on('finish', resolve);
+    });
+  }
+
+  del(id) {
+    return this.bucket.file(id).delete();
+  }
+
+  ping() {
+    return this.bucket.exists();
+  }
+}
+
+module.exports = GCSStorage;
