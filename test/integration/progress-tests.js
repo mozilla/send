d887ef21dab793a9c0bdd3fed5ffa8b4f076183c
Danny Coates <dannycoates@gmail.com>
2018-10-31 11:31:17 -0700

    updated integration tests for new ui

--- test/integration/progress-tests.js
+++ test/integration/progress-tests.js
@@ -1,22 +1,18 @@
-/* global browser document */
+/* global browser */
 const assert = require('assert');
-const ProgressPage = require('./pages/desktop/progress_page');
 const HomePage = require('./pages/desktop/home_page');
 
 describe('Firefox Send progress page', function() {
+  const homePage = new HomePage();
   beforeEach(function() {
-    browser.url('/');
+    homePage.open();
   });
 
-  it('should show an icon while an upload is in progress', function() {
-    browser.execute(() => {
-      document.getElementById('file-upload').style.display = 'block';
-    });
-    browser.waitForExist('#file-upload');
-    const homePage = new HomePage();
-    browser.chooseFile('#file-upload', __filename);
-    browser.click(homePage.readyToSend);
-    const progressPage = new ProgressPage();
-    assert.ok(progressPage.waitForPageToLoad());
+  it('should show progress when a file is uploading', function() {
+    browser.chooseFile(homePage.uploadInput, __filename);
+    browser.waitForExist(homePage.uploadButton);
+    browser.click(homePage.uploadButton);
+
+    assert.ok(browser.waitForExist(homePage.progress));
   });
 });
