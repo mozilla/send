5ebe869c265b5204aad8d01269684824f47df7bb
Danny Coates <dannycoates@gmail.com>
2018-11-01 12:04:14 -0700

    fixed intro size on home page

--- app/ui/home.js
+++ app/ui/home.js
@@ -8,28 +8,26 @@ module.exports = function(state, emit) {
   const archives = state.storage.files.map(archive =>
     archiveTile(state, emit, archive)
   );
-  let wip = '';
+  let left = '';
   if (state.uploading) {
-    wip = archiveTile.uploading(state, emit);
+    left = archiveTile.uploading(state, emit);
   } else if (state.archive) {
-    wip = archiveTile.wip(state, emit);
+    left = archiveTile.wip(state, emit);
   } else {
-    wip = archiveTile.empty(state, emit);
+    left = archiveTile.empty(state, emit);
   }
   archives.reverse();
-  if (archives.length < 1) {
-    archives.push(intro(state));
-  }
+  const right =
+    archives.length < 1
+      ? intro(state)
+      : list(archives, 'list-reset h-full overflow-y-scroll', 'pb-4');
+
   return html`
   <main class="main container md:relative">
     ${state.modal && modal(state, emit)}
     <section class="h-full w-full px-6 md:flex md:flex-row">
-      <div class="pt-4 md:pb-4 md:mr-6 md:w-1/2">${wip}</div>
-      ${list(
-        archives,
-        'list-reset h-full md:w-1/2 overflow-y-scroll pt-2',
-        'py-2'
-      )}
+      <div class="pt-4 md:pb-4 md:mr-6 md:w-1/2">${left}</div>
+      <div class="py-4 md:w-1/2">${right}</div>
     </section>
   </main>`;
 };
