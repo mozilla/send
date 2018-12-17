c71ef9ae842e72d2e9c76d8c7541c4cc0f3186fb
Donovan Preston <donovanpreston@gmail.com>
2018-12-17 16:40:17 -0500

    Fix #890  Fix delete by initializing the metrics on android; delete was trying to use metrics and causing an exception because it hadn't been initialized

--- app/controller.js
+++ app/controller.js
@@ -74,6 +74,7 @@ export default function(state, emitter) {
   });
 
   emitter.on('delete', async ({ file, location }) => {
+    console.log('ondelete');
     try {
       metrics.deletedUpload({
         size: file.size,
@@ -86,6 +87,7 @@ export default function(state, emitter) {
       state.storage.remove(file.id);
       await file.del();
     } catch (e) {
+      console.log(e.stack);
       state.raven.captureException(e);
     }
     render();
