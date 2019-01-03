5e5af03d1151161069800b6de62c6c7ce8f274ce
Danny Coates <dannycoates@gmail.com>
2019-01-03 13:56:32 -0800

    run npm install if needed in buildAssets.sh

--- android/app/buildAssets.sh
+++ android/app/buildAssets.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-
+if [ -d "../../node_modules" ]
+then
+  echo "node_modules already present."
+else
+  echo "node_modules not present, running npm install."
+  npm install
+fi
 npm run build
 rm -rf src/main/assets
 mkdir -p src/main/assets
