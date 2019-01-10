52b4c382cb5655de434458dc961a7b5af8dcc5ec
Donovan Preston <donovanpreston@gmail.com>
2019-01-10 15:08:16 -0500

    Make ANDROID=1 npm start work again, as it broke at some point

--- server/bin/dev.js
+++ server/bin/dev.js
@@ -21,7 +21,10 @@ module.exports = function(app, devServer) {
     const index = devServer.middleware.fileSystem
       .readFileSync(devServer.middleware.getFilenameFromUrl('/android.html'))
       .toString()
-      .replace('<base href="file:///android_asset/" />', '');
+      .replace(
+        '<base href="file:///android_asset/" />',
+        '<base href="http://localhost:8080/" />'
+      );
     res.set('Content-Type', 'text/html');
     res.send(index);
   }
@@ -30,7 +33,7 @@ module.exports = function(app, devServer) {
     app.get('/', android);
     app.get('/legal', android);
     app.get(`/share/:id${ID_REGEX}`, android);
-    app.get(`/download/:id${ID_REGEX}`, android);
+    app.get(`/download/:id${ID_REGEX}/:key`, android);
     app.get('/completed', android);
     app.get('/preferences', android);
     app.get('/options', android);
