4d8f27e96e8160951a832c7b7a84573ae4c5e476
Danny Coates <dannycoates@gmail.com>
2019-02-14 09:33:49 -0800

    adjusted intro layout

--- app/ui/intro.js
+++ app/ui/intro.js
@@ -3,20 +3,18 @@ const assets = require('../../common/assets');
 
 module.exports = function intro(state) {
   return html`
-  <send-intro class="flex flex-col items-center justify-center bg-white border border-grey-light md:border-none px-6 text-center md:py-0 py-6 mb-0 h-full w-full">
-    <div class="flex flex-col items-center justify-between h-full py-8">
-      <p class="text-center">
-        <div class="font-semibold leading-normal">${state.translate(
-          'uploadPageHeader'
-        )}</div>
-        <div class="italic text-sm opacity-75 leading-normal">${state.translate(
-          'pageHeaderCredits'
-        )}</div>
-      </p>
-      <img class="my-6" src="${assets.get('illustration_download.svg')}"/>
-      <p class="md:mx-6 max-w-sm text-sm opacity-50 leading-normal">${state.translate(
-        'uploadPageExplainer'
-      )}</p>
-    </div>
-  </send-intro>`;
+    <send-intro
+      class="flex flex-col items-center justify-center bg-white border border-grey-light md:border-none px-6 md:py-0 py-6 mb-0 h-full w-full"
+    >
+      <div class="flex flex-col justify-end h-full py-8">
+        <h1 class="font-bold leading-normal">
+          ${state.translate('uploadPageHeader')}
+        </h1>
+        <p class="max-w-sm font-light leading-normal">
+          ${state.translate('uploadPageExplainer')}
+        </p>
+        <img class="my-6" src="${assets.get('illustration_download.svg')}" />
+      </div>
+    </send-intro>
+  `;
 };
