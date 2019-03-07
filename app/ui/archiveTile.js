ea78382e5e3931bfccd9bd2341e3a7103e9c183e
Danny Coates <dannycoates@gmail.com>
2019-03-07 10:58:19 -0800

    fixes #1204, fixes #1205, also intro font weight

--- app/ui/archiveTile.js
+++ app/ui/archiveTile.js
@@ -422,9 +422,7 @@ module.exports.empty = function(state, emit) {
       }}"
     >
       <img src="${assets.get('addfiles.svg')}" width="48" height="48" />
-      <div
-        class="pt-6 pb-2 text-center text-lg font-bold capitalize tracking-wide"
-      >
+      <div class="pt-6 pb-2 text-center text-lg font-bold tracking-wide">
         ${state.translate('dragAndDropFiles')}
       </div>
       <div class="pb-6 text-center text-base">
--- app/ui/intro.js
+++ app/ui/intro.js
@@ -6,11 +6,11 @@ module.exports = function intro(state) {
     <send-intro
       class="flex flex-col items-center justify-center bg-white px-6 md:py-0 py-6 mb-0 h-full w-full"
     >
-      <div class="flex flex-col justify-end h-full">
+      <div class="flex flex-col h-full">
         <h1 class="font-bold">
           ${state.translate('introTitle')}
         </h1>
-        <p class="max-w-sm mt-2 pr-16 font-light leading-normal">
+        <p class="max-w-sm mt-2 pr-16 leading-normal">
           ${state.translate('introDescription')}
         </p>
         <img class="intro" src="${assets.get('intro.svg')}" />
