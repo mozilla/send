fc1d2f43dae7850f9250cb023cb90e2ac8f51fa9
Danny Coates <dannycoates@gmail.com>
2019-03-07 11:17:27 -0800

    added top margin to intro

--- app/ui/intro.js
+++ app/ui/intro.js
@@ -6,7 +6,7 @@ module.exports = function intro(state) {
     <send-intro
       class="flex flex-col items-center justify-center bg-white px-6 md:py-0 py-6 mb-0 h-full w-full"
     >
-      <div class="flex flex-col h-full">
+      <div class="mt-12 flex flex-col h-full">
         <h1 class="font-bold">
           ${state.translate('introTitle')}
         </h1>
