7a73f4e4c5d087b0268f2a9850a29e33c28bf4c2
oliver-dvorski <oliver.dvorski@gmail.com>
2019-05-06 22:06:36 +0200

    Display *a* qr code

--- app/ui/displayQR.js
+++ app/ui/displayQR.js
@@ -1,12 +1,22 @@
 const html = require('choo/html');
+const QRious = require('qrious');
 
 module.exports = function() {
+  const qr = new QRious({
+    value: 'https://oliverdvorski.com',
+    size: 300
+  });
+
   return html`
     <main class="main">
       <section
-        class="h-full w-full p-6 md:p-8 md:flex md:flex-row md:rounded-xl md:shadow-big"
+        class="h-full w-full p-6 md:p-8 flex flex-col md:rounded-xl md:shadow-big"
       >
-        Here's a QR Code
+        <div>
+          <img class="max-w-100" src="${qr.toDataURL('image/jpeg')}" />
+        </div>
+
+        <div>column</div>
       </section>
     </main>
   `;
