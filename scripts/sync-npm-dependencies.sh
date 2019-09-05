9ad9c9feb2892b1879baa8dcf12ad5eee3c114a1
Danny Coates <dannycoates@gmail.com>
2019-09-05 13:16:32 -0700

    fixed git hooks

--- scripts/sync-npm-dependencies.sh
+++ scripts/sync-npm-dependencies.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo "checking package-lock.json for changes"
-if [ "$1" != "$2" ]; then
-  DIFF=$(git diff $1 $2 package-lock.json)
+IFS=' '
+read -ra G_PARAMS <<< "$HUSKY_GIT_PARAMS"
+PREV=${G_PARAMS[0]}
+NEXT=${G_PARAMS[1]}
+if [ "$PREV" != "$NEXT" ]; then
+  DIFF=$(git diff $PREV $NEXT package-lock.json)
   if [ "$DIFF" != "" ]; then
     npm install
   fi
