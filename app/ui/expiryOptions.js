7d348e648dd35fbdb1c9f34c6392964c886ff0a5
charli-watson <charli.watson@checkr.com>
2019-12-17 15:33:20 -0700

    Fix JS error that may be breaking dropdowns, small CSS fix for upload button, and don't redirect to firefox signup

--- app/ui/expiryOptions.js
+++ app/ui/expiryOptions.js
@@ -33,11 +33,7 @@ module.exports = function(state, emit) {
       value => {
         const max = state.user.maxDownloads;
         state.archive.dlimit = Math.min(value, max);
-        if (value > max) {
-          emit('signup-cta', 'count');
-        } else {
-          emit('render');
-        }
+        emit('render');
       },
       'expire-after-dl-count-select'
     ),
@@ -60,11 +56,7 @@ module.exports = function(state, emit) {
       value => {
         const max = state.user.maxExpireSeconds;
         state.archive.timeLimit = Math.min(value, max);
-        if (value > max) {
-          emit('signup-cta', 'time');
-        } else {
-          emit('render');
-        }
+        emit('render');
       },
       'expire-after-time-select'
     ),
